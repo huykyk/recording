91945447340714d98b2371022529e3bdab4a3ee7
huykyk <1073157267@qq.com>
2018-01-15 03:17:59 +0800

    对于数组index的应用

--- leecode/类别/tp100/283.java
+++ leecode/类别/tp100/283.java
@@ -14,3 +14,18 @@ Special thanks to @jianchao.li.fighter for adding this problem and creating all 
 
 */
 
+class Solution {
+    public void moveZeroes(int[] nums) {
+        
+        int index = 0;
+        for(int i = 0;i< nums.length;i++){
+            if(nums[i]!=0){
+                nums[index++] = nums[i];
+            }
+        }
+        for(int i=index;i<nums.length;i++){
+            nums[i] = 0;
+        }
+    }
+}
+
