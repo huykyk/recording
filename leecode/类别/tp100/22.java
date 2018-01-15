0fb6ff1545957422115933c3541b89f185a45a8f
huykyk <1073157267@qq.com>
2018-01-16 03:59:22 +0800

    排序 暴力大法好
    Set

--- leecode/类别/tp100/22.java
+++ leecode/类别/tp100/22.java
@@ -14,4 +14,17 @@ For example, given n = 3, a solution set is:
   "()()()"
 ]
 */
-
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> res = new ArrayList<String>();
+        if(n>0) genereParenthesisFirst("",n,n,res);
+        return res; 
+    }
+        private void genereParenthesisFirst(String pre,int left,int right,List<String> res)
+        {
+            if(left==0&&right==0) res.add(pre);
+            if(left>0) genereParenthesisFirst(pre+'(',left-1,right,res);
+            if(left<right) genereParenthesisFirst(pre+')',left,right-1,res);
+        }
+   
+}
